7802dc42415982b7ef1f193280a9f77f77a938eb
jihae <sojihae99@gmail.com>
2022-03-08 18:30:22 +0900

    BOJ_2675_solved

--- BOJ_2675.py
+++ BOJ_2675.py
@@ -1,20 +1,7 @@
-#테스트 케이스의 개수 조건
-T = int(input())
-1<= T <= 1000
+n  = int(input())
 
-#반복횟수의 조건 
-
-R = int(input())
-1<= R<=8
-
-
-word = str(input().split())
-
-
-#코드
-for n in range(len(word)):
-    for x in word:
-        print(x*R,end="")
-print()
-
-``
+for _ in range (n) :
+    r, word = input().split()
+    for words in word:
+        print(words*int(r),end='')
+    print()
